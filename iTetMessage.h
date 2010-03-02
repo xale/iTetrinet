9d0543f33553c21d0d802168119fbac0a1a1e180
Alex Heinz <xale@acm.jhu.edu>
2010-03-02 15:10:17 -0500

    Added an (incomplete) enum of message types.

--- iTetMessage.h
+++ iTetMessage.h
@@ -7,9 +7,43 @@
 
 #import <Cocoa/Cocoa.h>
 
+#define NUM_MESSAGE_TYPES	
+
+typedef enum
+{
+	playerNumberMessage,
+	playerJoinMessage,
+	playerLeaveMessage,
+	playerTeamMessage,
+	winlistMessage,
+	plineChatMessage,
+	plineActionMessage,
+	gameChatMessage,
+	newGameMessage,
+	inGameMessage,
+	fieldstringMessage,
+	levelUpdateMessage,
+	specialUsedMessage,
+	classicAddMessage,
+	playerLostMessage,
+	playerWonMessage,
+	pauseGameMessage,
+	endGameMessage,
+	noConnectingMessage,
+	heartbeatMessage
+} iTetMessageType;
+
 @interface iTetMessage : NSObject
 {
-	
+	iTetMessageType messageType;
 }
+
++ (id)messageFromData:(NSData*)data; 
+
+- (id)messageFromData:(NSData*)data;
+
+- (NSData*)rawMessageData;
+
+@property (readonly) iTetMessageType messageType;
 
 @end
