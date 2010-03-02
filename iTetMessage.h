f26dd55a1e7cd934386b0027fa9db36320c9b5af
Alex Heinz <xale@acm.jhu.edu>
2010-03-02 13:03:04 -0500

    Added an abstract Message superclass.

---
+++ iTetMessage.h
@@ -0,0 +1,15 @@
+//
+//  iTetMessage.h
+//  iTetrinet
+//
+//  Created by Alex Heinz on 3/2/10.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface iTetMessage : NSObject
+{
+	
+}
+
+@end
---
+++ iTetMessage.m
@@ -0,0 +1,19 @@
+//
+//  iTetMessage.m
+//  iTetrinet
+//
+//  Created by Alex Heinz on 3/2/10.
+//
+
+#import "iTetMessage.h"
+
+@implementation iTetMessage
+
+- (id)init
+{
+	[self doesNotRecognizeSelector:_cmd];
+	[self release];
+	return nil;
+}
+
+@end
