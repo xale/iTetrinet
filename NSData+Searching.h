b6e14f33fb40ff8a99510ed2e746bc793d8f7a84
Alex Heinz <xale@acm.jhu.edu>
2010-03-10 20:38:03 -0500

    Added some documentation comments to the utility categories.

--- NSData+Searching.h
+++ NSData+Searching.h
@@ -9,9 +9,13 @@
 
 @interface NSData (Searching)
 
+// Returns the index of the first occurrence of the specified byte in the receiver, or NSNotFound if the byte is not present in the data
 - (NSUInteger)indexOfByte:(uint8_t)byte;
 
+// Returns an NSData object containing the bytes of the receiver up to and excluding the specified index.
 - (NSData*)subdataToIndex:(NSUInteger)index;
+
+// Returns an NSData object containing the bytes of the receiver from the specified index to the end, inclusive.
 - (NSData*)subdataFromIndex:(NSUInteger)index;
 
 @end
--- NSString+ASCIIData.h
+++ NSString+ASCIIData.h
@@ -9,6 +9,7 @@
 
 @interface NSString (ASCIIData)
 
+// Returns the ASCII-encoded representation of the provided data as an NSString, or nil if the data cannot be encoded as ASCII; 'asciiData' does _not_ need to be null-terminated
 + (id)stringWithASCIIData:(NSData*)asciiData;
 
 @end
