804ebf4acabdb648dba0e8ae01a5476f5a3c1271
Alex Heinz <xale@acm.jhu.edu>
2010-03-10 22:39:45 -0500

    Merged category documentation comments from branch 'message-rewrite'.

--- NSMutableData+SingleByte.h
+++ NSMutableData+SingleByte.h
@@ -9,8 +9,10 @@
 
 @interface NSMutableData (SingleByte)
 
+// Appends the specified byte to the end of the receiver's bytes
 - (void)appendByte:(uint8_t)byte;
 
+// Inserts the specified byte in the receiver's bytes at the specified index, shifting the following bytes
 - (void)insertByte:(uint8_t)byte
 		   atIndex:(NSUInteger)index;
 
