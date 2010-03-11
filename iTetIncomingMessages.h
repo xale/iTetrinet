346ac55c3af829072d49c7d255e9a6bf6457735d
Alex Heinz <xale@acm.jhu.edu>
2010-03-11 00:13:08 -0500

    Added IncomingMessages and OutgoingMessages header files, to allow easier #imports of all the Message subclasses.

---
+++ iTetIncomingMessages.h
@@ -0,0 +1,26 @@
+//
+//  iTetIncomingMessage.h
+//  iTetrinet
+//
+//  Created by Alex Heinz on 3/10/10.
+//
+
+#import "iTetNoConnectingMessage.h"
+#import "iTetClientInfoRequestMessage.h"
+#import "iTetHeartbeatMessage.h"
+#import "iTetPlayerNumberMessage.h"
+#import "iTetPlayerJoinMessage.h"
+#import "iTetPlayerLeaveMessage.h"
+#import "iTetPlayerTeamMessage.h"
+#import "iTetWinlistMessage.h"
+#import "iTetPlineActionMessage.h"
+#import "iTetGameChatMessage.h"
+#import "iTetNewGameMessage.h"
+#import "iTetInGameMessage.h"
+#import "iTetPauseResumeGameMessage.h"
+#import "iTetEndGameMessage.h"
+#import "iTetFieldstringMessage.h"
+#import "iTetLevelUpdateMessage.h"
+#import "iTetSpecialMessage.h"
+#import "iTetPlayerLostMessage.h"
+#import "iTetPlayerWonMessage.h"
