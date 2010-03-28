9a44cd3485543185ba2aed046e9d69c7fc04c321
Alex Heinz <xale@acm.jhu.edu>
2010-03-28 12:46:52 -0400

    Added a subclass of NSLevelIndicator that can be scaled in the vertical dimension.

---
+++ IPSScalableLevelIndicator.h
@@ -0,0 +1,12 @@
+//
+//  IPSScalableLevelIndicator.h
+//  iTetrinet
+//
+//  Created by Alex Heinz on 3/28/10.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IPSScalableLevelIndicator : NSLevelIndicator
+
+@end
---
+++ IPSScalableLevelIndicator.m
@@ -0,0 +1,38 @@
+//
+//  IPSScalableLevelIndicator.m
+//  iTetrinet
+//
+//  Created by Alex Heinz on 3/28/10.
+//
+
+#import "IPSScalableLevelIndicator.h"
+
+#define DEFAULT_NSLEVELINDICATOR_HEIGHT	(18.0)
+
+@implementation IPSScalableLevelIndicator
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+	// Get the graphics context we are drawing to
+	NSGraphicsContext* graphicsContext = [NSGraphicsContext currentContext];
+	
+	// Push the existing context onto the context stack
+	[graphicsContext saveGraphicsState];
+	
+	// Create an affine transform from the default NSLevelIndicator height to this view's height
+	CGFloat viewHeight = [self frame].size.height;
+	NSAffineTransform* scaleTransform = [NSAffineTransform transform];
+	[scaleTransform scaleXBy:1.0
+						 yBy:(viewHeight / DEFAULT_NSLEVELINDICATOR_HEIGHT)];
+	
+	// Concatenate the transform to the graphics context
+	[scaleTransform concat];
+	
+	// Call the NSLevelIndicator -drawRect: with the transformed graphics context
+	[super drawRect:dirtyRect];
+	
+	// Pop the graphics context
+	[graphicsContext restoreGraphicsState];
+}
+
+@end
