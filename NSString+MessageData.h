2a0cbc95dba4842da09ff441e7df45afabd4ce92
Alex Heinz <xale@acm.jhu.edu>
2010-04-08 23:49:47 -0400

    Added a mechanism for falling back to ISO-8859-1 in the event that reading data from the server as UTF-8 fails.

--- NSString+MessageData.h
+++ NSString+MessageData.h
@@ -9,8 +9,12 @@
 
 @interface NSString (MessageData)
 
-// Returns the representation of the data provided from a message as an NSString; the data does _not_ need to be null-terminated
-+ (id)stringWithMessageData:(NSData*)data;
+// Returns the representation of the data provided from a message as an autoreleased NSString; the data does _not_ need to be null-terminated
++ (id)stringWithMessageData:(NSData*)messageData;
+
+// Returns an autoreleased NSString containing the specified data interpreted using the specified encoding; the data need not be null-terminated
++ (id)stringWithData:(NSData*)data
+			encoding:(NSStringEncoding)encoding;
 
 // Returns an NSData object containing the receiver's contents encoded in a format suitable for sending in a message
 - (NSData*)messageData;
--- NSString+MessageData.m
+++ NSString+MessageData.m
@@ -9,11 +9,25 @@
 
 @implementation NSString (MessageData)
 
-+ (id)stringWithMessageData:(NSData*)data
++ (id)stringWithMessageData:(NSData*)messageData
+{
+	// First, attempt to interpret the string as UTF-8
+	NSString* utf8String = [self stringWithData:messageData
+									   encoding:NSUTF8StringEncoding];
+	if (utf8String != nil)
+		return utf8String;
+	
+	// If that fails, decode the string as ISO-8859-1
+	return [self stringWithData:messageData
+					   encoding:NSISOLatin1StringEncoding];
+}
+
++ (id)stringWithData:(NSData*)data
+			encoding:(NSStringEncoding)encoding
 {
 	return [[[self alloc] initWithBytes:[data bytes]
 								 length:[data length]
-							   encoding:NSUTF8StringEncoding] autorelease];
+							   encoding:encoding] autorelease];
 }
 
 - (NSData*)messageData
