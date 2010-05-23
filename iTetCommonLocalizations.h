71cca018ac61e6474099f8eebd558708f3fce234
Alex Heinz <xale@acm.jhu.edu>
2010-05-23 13:33:25 -0400

    Added a header file containing internationalizations of strings common to many dialogs and buttons, like 'Cancel' or 'Save'.

---
+++ iTetCommonLocalizations.h
@@ -0,0 +1,9 @@
+//
+//	iTetCommonLocalizations.h
+//	iTetrinet
+//	
+//	Created by Alex Heinz on 5/23/10.
+//
+
+#define iTetCancelButtonTitle	NSLocalizedStringFromTable(@"Cancel", @"Common", @"Title of 'cancel' button for general alert dialogs")
+#define iTetDeleteButtonTitle	NSLocalizedStringFromTable(@"Delete", @"Common", @"Title of 'delete' button on 'confirm deletion?' alert dialogs")
