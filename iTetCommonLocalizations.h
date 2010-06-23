6ef33ecc975069246bc1a0f4b7880ae1b24a9968
Alex Heinz <xale@acm.jhu.edu>
2010-06-22 21:13:02 -0400

    [Minor] changed a comment on one of the common string internationalization macros.

--- iTetCommonLocalizations.h
+++ iTetCommonLocalizations.h
@@ -12,7 +12,7 @@
 #define iTetDeleteButtonTitle			NSLocalizedStringFromTable(@"Delete", @"Common", @"Title of 'delete' button on 'confirm deletion?' alert dialogs")
 #define iTetDoNotSaveButtonTitle		NSLocalizedStringFromTable(@"Don't Save", @"Common", @"Title of 'don't save' button on 'confirm quit without saving?' alert dialogs")
 #define iTetReplaceButtonTitle			NSLocalizedStringFromTable(@"Replace", @"Common", @"Title of 'replace' button on 'replace existing item?' alert dialogs")
-#define iTetContinuePlayingButtonTitle	NSLocalizedStringFromTable(@"Continue Playing", @"Common", @"Title of button on 'leave with game in progress?' alert that allows the the user to cancel and continue playing the game")
+#define iTetContinuePlayingButtonTitle	NSLocalizedStringFromTable(@"Continue Playing", @"Common", @"Title of button on 'leave with game in progress?' alerts that allows the the user to cancel and continue playing the game")
 
 #pragma mark Alert Titles
 #define iTetGameInProgressAlertTitle	NSLocalizedStringFromTable(@"Game in Progress", @"Common", @"Title of alert displayed when the user attempts to disconnect from a server or close the application while participating in a game")
