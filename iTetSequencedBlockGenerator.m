4e6a6488f870dcfbdcc20ae73dfe8f0d21ab0412
Alex Heinz <xale@acm.jhu.edu>
2010-07-12 14:49:24 -0400

    Added additional casts to the constants in the linear-congruential block generator PRNG, to prevent overflows on 32-bit architectures.

--- iTetSequencedBlockGenerator.m
+++ iTetSequencedBlockGenerator.m
@@ -24,24 +24,24 @@
 	return self;
 }
 
-#define ITET_GENERATOR_MULTIPLIER				0x08088405UL
-#define ITET_GENERATOR_INCREMENT				0x00000001UL
-#define ITET_GENERATOR_MODULUS					(0x1UL << 32)
+#define ITET_GENERATOR_MULTIPLIER				((uint64_t)(0x08088405))
+#define ITET_GENERATOR_INCREMENT				((uint64_t)(0x00000001))
+#define ITET_GENERATOR_MODULUS					(((uint64_t)(0x1)) << 32)
 
 #define ITET_GENERATOR_NEXT_SEQUENCE_VALUE(v)	(((((uint64_t)(v)) * ITET_GENERATOR_MULTIPLIER) + ITET_GENERATOR_INCREMENT) % ITET_GENERATOR_MODULUS)
 
-#define ITET_GENERATOR_TYPE_INDEXES	100UL
-#define ITET_GENERATOR_ORIENTATIONS	4UL
+#define ITET_GENERATOR_NUM_TYPE_INDEXES			((uint64_t)(100))
+#define ITET_GENERATOR_NUM_ORIENTATIONS			((uint64_t)(4))
 
 - (iTetBlock*)generateNextBlock
 {
 	// Generate the next pseudorandom value in the sequence, and use it to generate an index in the block frequencies array
 	sequenceValue = ITET_GENERATOR_NEXT_SEQUENCE_VALUE(sequenceValue);
-	NSUInteger blockTypeIndex = ((((uint64_t)sequenceValue) * ITET_GENERATOR_TYPE_INDEXES) / ITET_GENERATOR_MODULUS);
+	NSUInteger blockTypeIndex = ((((uint64_t)sequenceValue) * ITET_GENERATOR_NUM_TYPE_INDEXES) / ITET_GENERATOR_MODULUS);
 	
 	// Generate another value, and use it to determine the block's orientation
 	sequenceValue = ITET_GENERATOR_NEXT_SEQUENCE_VALUE(sequenceValue);
-	NSUInteger blockOrientation = ((((uint64_t)sequenceValue) * ITET_GENERATOR_ORIENTATIONS) / ITET_GENERATOR_MODULUS);
+	NSUInteger blockOrientation = ((((uint64_t)sequenceValue) * ITET_GENERATOR_NUM_ORIENTATIONS) / ITET_GENERATOR_MODULUS);
 	
 	// Create and return a new block using the block type at the generated index, and the generated orientation
 	return [iTetBlock blockWithType:(iTetBlockType)[[blockFrequencies objectAtIndex:blockTypeIndex] intValue]
