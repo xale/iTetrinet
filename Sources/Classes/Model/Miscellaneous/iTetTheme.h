5ce851cf027dc3f95e3b91c754fe74e2bc73cf0e
Alex Heinz <xale@acm.jhu.edu>
2011-01-23 16:17:11 -0500

    [Cosmetic] Credits and copyright update.

--- Sources/Classes/Model/Miscellaneous/iTetTheme.h
+++ Sources/Classes/Model/Miscellaneous/iTetTheme.h
@@ -3,7 +3,7 @@
 //  iTetrinet
 //
 //  Created by Alex Heinz on 6/5/09.
-//  Copyright (c) 2009-2010 Alex Heinz (xale@acm.jhu.edu)
+//  Copyright (c) 2009-2011 Alex Heinz (xale@acm.jhu.edu)
 //  This is free software, presented under the MIT License
 //  See the included license.txt for more information
 //
@@ -21,13 +21,13 @@
 	NSString* themeName;
 	NSString* themeAuthor;
 	NSString* themeDescription;
+	NSImage* preview;
 	
+	NSSize cellSize;
 	NSImage* localFieldBackground;
 	NSImage* remoteFieldBackground;
-	NSSize cellSize;
-	NSArray* cellImages;
-	NSArray* specialImages;
-	NSImage* preview;
+	NSArray* localCellImages;
+	NSArray* localSpecialImages;
 }
 
 + (iTetTheme*)currentTheme;
@@ -47,13 +47,11 @@
 @property (readonly) NSString* themeName;
 @property (readonly) NSString* themeAuthor;
 @property (readonly) NSString* themeDescription;
-
-@property (readonly) NSImage* localFieldBackground;
-@property (readonly) NSImage* remoteFieldBackground;
+@property (readonly) NSImage* preview;
 
 @property (readonly) NSSize cellSize;
+@property (readonly) NSImage* localFieldBackground;
+@property (readonly) NSImage* remoteFieldBackground;
 - (NSImage*)imageForCellType:(uint8_t)cellType;
-
-@property (readonly) NSImage* preview;
 
 @end
