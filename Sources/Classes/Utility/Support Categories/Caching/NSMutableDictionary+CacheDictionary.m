1f5555170b9b9b84c04e76aaaf1d5e63902a1dc4
Alex Heinz <xale@acm.jhu.edu>
2011-01-23 16:18:24 -0500

    [Cosmetic] More copyright updates.

--- Sources/Classes/Utility/Support Categories/Caching/NSMutableDictionary+CacheDictionary.m
+++ Sources/Classes/Utility/Support Categories/Caching/NSMutableDictionary+CacheDictionary.m
@@ -2,7 +2,7 @@
 //  NSMutableDictionary+CacheDictionary.m
 //
 //  Created by Alex Heinz on 1/19/09.
-//  Copyright (c) 2009-2010 Alex Heinz (xale@acm.jhu.edu)
+//  Copyright (c) 2009-2011 Alex Heinz (xale@acm.jhu.edu)
 //  This is free software, presented under the MIT License
 //  See the included license.txt for more information
 //
