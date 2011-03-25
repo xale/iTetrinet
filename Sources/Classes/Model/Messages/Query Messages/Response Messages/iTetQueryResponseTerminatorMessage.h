786f5698cb13ea4075f247080f337acc6818151c
Alex Heinz <xale@acm.jhu.edu>
2011-03-24 22:17:45 -0700

    Added a Message subclass for query-response-terminator messages.

---
+++ Sources/Classes/Model/Messages/Query Messages/Response Messages/iTetQueryResponseTerminatorMessage.h
@@ -0,0 +1,17 @@
+//
+//  iTetQueryResponseTerminatorMessage.h
+//  iTetrinet
+//
+//  Created by Alex Heinz on 3/19/11.
+//  Copyright (c) 2011 Alex Heinz (xale@acm.jhu.edu)
+//  This is free software, presented under the MIT License
+//  See the included license.txt for more information
+//
+
+#import "iTetNoPayloadMessage.h"
+
+#define iTetQueryResponseTerminatorMessageTokenCount		1
+
+@interface iTetQueryResponseTerminatorMessage : iTetNoPayloadMessage
+
+@end
---
+++ Sources/Classes/Model/Messages/Query Messages/Response Messages/iTetQueryResponseTerminatorMessage.m
@@ -0,0 +1,35 @@
+//
+//  iTetQueryResponseTerminatorMessage.m
+//  iTetrinet
+//
+//  Created by Alex Heinz on 3/19/11.
+//  Copyright (c) 2011 Alex Heinz (xale@acm.jhu.edu)
+//  This is free software, presented under the MIT License
+//  See the included license.txt for more information
+//
+
+#import "iTetQueryResponseTerminatorMessage.h"
+
+NSString* const iTetQueryResponseTerminatorMessageContents =	@"+OK";
+
+@implementation iTetQueryResponseTerminatorMessage
+
+- (id)initWithMessageTokens:(NSArray*)messageTokens
+{
+	// Check the length of the list of tokens, and that the contents are as expected
+	if (([messageTokens count] != iTetQueryResponseTerminatorMessageTokenCount) || ![[messageTokens objectAtIndex:0] isEqualToString:iTetQueryResponseTerminatorMessageContents])
+	{
+		[self release];
+		return nil;
+	}
+	
+	// Valid query-response terminator
+	return self;
+}
+
+- (NSString*)messageContents
+{
+	return iTetQueryResponseTerminatorMessageContents;
+}
+
+@end
