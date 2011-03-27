488c517475afeb4ad79d61698ccee5da75ed1923
Alex Heinz <xale@acm.jhu.edu>
2011-03-27 12:09:20 -0700

    Added a Game Context subclass for offline games.

---
+++ Sources/Classes/Model/Game Contexts/iTetOfflineGameContext.h
@@ -0,0 +1,18 @@
+//
+//  iTetOfflineGameContext.h
+//  iTetrinet
+//
+//  Created by Alex Heinz on 3/16/11.
+//  Copyright (c) 2011 Alex Heinz (xale@acm.jhu.edu)
+//  This is free software, presented under the MIT License
+//  See the included license.txt for more information
+//
+
+#import "iTetGameContext.h"
+
+@interface iTetOfflineGameContext : iTetGameContext
+{
+
+}
+
+@end
---
+++ Sources/Classes/Model/Game Contexts/iTetOfflineGameContext.m
@@ -0,0 +1,25 @@
+//
+//  iTetOfflineGameContext.m
+//  iTetrinet
+//
+//  Created by Alex Heinz on 3/16/11.
+//  Copyright (c) 2011 Alex Heinz (xale@acm.jhu.edu)
+//  This is free software, presented under the MIT License
+//  See the included license.txt for more information
+//
+
+#import "iTetOfflineGameContext.h"
+
+@implementation iTetOfflineGameContext
+
+#pragma mark -
+#pragma mark Accessors
+
+#pragma mark Overrides
+
+- (BOOL)isOfflineContext
+{
+	return YES;
+}
+
+@end
