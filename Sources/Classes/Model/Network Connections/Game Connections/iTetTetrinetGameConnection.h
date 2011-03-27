632d61934f2a086f7c54f9ac44644dc158adf0c9
Alex Heinz <xale@acm.jhu.edu>
2011-03-26 23:52:57 -0700

    Added a Game Connection subclass for the TetriNET protocol.

---
+++ Sources/Classes/Model/Network Connections/Game Connections/iTetTetrinetGameConnection.h
@@ -0,0 +1,15 @@
+//
+//  iTetTetrinetGameConnection.h
+//  iTetrinet
+//
+//  Created by Alex Heinz on 3/21/11.
+//  Copyright (c) 2011 Alex Heinz (xale@acm.jhu.edu)
+//  This is free software, presented under the MIT License
+//  See the included license.txt for more information
+//
+
+#import "iTetGameConnection.h"
+
+@interface iTetTetrinetGameConnection : iTetGameConnection
+
+@end
---
+++ Sources/Classes/Model/Network Connections/Game Connections/iTetTetrinetGameConnection.m
@@ -0,0 +1,80 @@
+//
+//  iTetTetrinetGameConnection.m
+//  iTetrinet
+//
+//  Created by Alex Heinz on 3/21/11.
+//  Copyright (c) 2011 Alex Heinz (xale@acm.jhu.edu)
+//  This is free software, presented under the MIT License
+//  See the included license.txt for more information
+//
+
+#import "iTetTetrinetGameConnection.h"
+
+#import "iTetNoConnectingMessage.h"
+
+#import "iTetTetrinetPlayerNumberMessage.h"
+#import "iTetPlayerJoinMessage.h"
+#import "iTetPlayerLeaveMessage.h"
+#import "iTetPlayerKickMessage.h"
+#import "iTetPlayerTeamMessage.h"
+#import "iTetWinlistMessage.h"
+
+#import "iTetPlayerLevelMessage.h"
+
+static NSDictionary* tetrinetMessageDictionary =	nil;
+
+@interface iTetTetrinetGameConnection(Private)
+
+- (NSDictionary*)tetrinetMessageDictionary;
+
+@end
+
+@implementation iTetTetrinetGameConnection
+
+- (NSDictionary*)messageTypesByTag
+{
+	@synchronized(self)
+	{
+		if (tetrinetMessageDictionary == nil)
+		{
+			tetrinetMessageDictionary = [[self tetrinetMessageDictionary] copy];
+		}
+	}
+	
+	return tetrinetMessageDictionary;
+}
+
+- (NSDictionary*)tetrinetMessageDictionary
+{
+	// Create a dictionary, and add the standard tetrinet protocol messages to it
+	NSMutableDictionary* messages = [NSMutableDictionary dictionary];
+	
+	// Connection status messages
+	[messages setObject:[iTetNoConnectingMessage class]
+				 forKey:iTetNoConnectingMessageTag];
+	// FIXME: WRITEME
+	
+	// Game context messages
+	[messages setObject:[iTetTetrinetPlayerNumberMessage class]
+				 forKey:iTetTetrinetPlayerNumberMessageTag];
+	[messages setObject:[iTetPlayerJoinMessage class]
+				 forKey:iTetPlayerJoinMessageTag];
+	[messages setObject:[iTetPlayerLeaveMessage class]
+				 forKey:iTetPlayerLeaveMessageTag];
+	[messages setObject:[iTetPlayerKickMessage class]
+				 forKey:iTetPlayerKickMessageTag];
+	[messages setObject:[iTetPlayerTeamMessage class]
+				 forKey:iTetPlayerTeamMessageTag];
+	[messages setObject:[iTetWinlistMessage class]
+				 forKey:iTetWinlistMessageTag];
+	// FIXME: WRITEME
+	
+	// Gameplay messages
+	[messages setObject:[iTetPlayerLevelMessage class]
+				 forKey:iTetPlayerLevelMessageTag];
+	// FIXME: WRITEME
+	
+	return messages;
+}
+
+@end
